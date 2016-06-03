98e6d9a0c33ce49b6ba37d20217cb45293bdc192
Hari Prasad V <hariv97@gmail.com>
2016-06-04 03:35:54 +0530

    Starting structure for path interpolation

---
+++ interpolation/src/main.cpp
@@ -0,0 +1,19 @@
+#include <interpolation/spline.h>
+
+int main(int argc, char** argv)
+{
+    SplineFit spFit(10);
+    std::vector<Pose> pts;
+    //Sample points for interpolation
+    for(int i=0;i<=20;i++)
+    {
+        Pose position;
+        position.x = i;
+        position.y = 0;
+        position.z = 100-(i-10)*(i-10);
+        pts.push_back(position);
+    }
+    std::vector<Pose> finalPts;
+    finalPts = spFit.interpolate(pts);
+    return 0;
+}
---
+++ interpolation/src/spline.cpp
@@ -0,0 +1,13 @@
+#include <interpolation/spline.h>
+
+SplineFit::SplineFit(int num, int deg)
+{
+    n = num;
+    degree = deg;
+}
+
+std::vector<Pose> SplineFit::interpolate(std::vector<Pose> pts)
+{
+    //TODO implement the interpolation
+    return path;
+}
