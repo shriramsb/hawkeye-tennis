c12450ba2582d28dbd0bf598f9824028eea4d3ca
shriram <sbshriram16@gmail.com>
2016-06-04 15:37:00 +0530

    fg objects by bg sub.

---
+++ hawkeye c++/avgsub/CMakeFiles/avgsub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/avgsub.dir/avgsub.cpp.o"
+  "avgsub.pdb"
+  "avgsub"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/avgsub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
